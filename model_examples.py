4bca84f2bdd232b7288ebe55b6e9862b355b9929
cluster <cluster@modernintelligence.ai>
2023-11-12 17:29:55 -0800

    Model Examples now creates 3 example braggnn models

--- model_examples.py
+++ model_examples.py
@@ -13,20 +13,20 @@ x = torch.randn((256,1,11,11))
 
 #High performing model found by search
 Blocks = nn.Sequential(
-    ConvBlock([24,16,16], [3,1], [nn.GELU(), nn.ReLU()], [None, 'batch']),
+    ConvBlock([24,16,16], [3,1], [nn.GELU(), nn.ReLU()], [None, 'batch'],img_size=9),
     ConvAttn(16,1),
-    ConvBlock([16,4,16], [3,3], [nn.GELU(), nn.GELU()], [None, 'batch'])
+    ConvBlock([16,4,16], [3,3], [nn.GELU(), nn.GELU()], [None, 'batch'],img_size=7)
 )
-mlp = MLP(widths=[1296, 64, 16, 4, 2], acts=[nn.GELU(), None, None, None], norms=['layer', 'layer','layer',None])
+mlp = MLP(widths=[144, 64, 16, 4, 2], acts=[nn.GELU(), None, None, None], norms=['layer', 'layer','layer',None])
 model = CandidateArchitecture(Blocks, mlp,24)
 y = model(x)
 
 #OpenHLS Model
 Blocks = nn.Sequential(
     ConvAttn(16,8, norm=None, act=nn.ReLU()),
-    ConvBlock([16,8,2], [3,3], [nn.ReLU(), nn.ReLU()], [None, None])
+    ConvBlock([16,8,2], [3,3], [nn.ReLU(), nn.ReLU()], [None, None],img_size=9)
 )
-mlp = MLP(widths=[2*9*9, 16, 8, 4, 2], acts=[nn.ReLU(), nn.ReLU(), nn.ReLU(), nn.ReLU()], norms=[None, None, None, None])
+mlp = MLP(widths=[50, 16, 8, 4, 2], acts=[nn.ReLU(), nn.ReLU(), nn.ReLU(), nn.ReLU()], norms=[None, None, None, None])
 model = CandidateArchitecture(Blocks, mlp,16)
 y = model(x)
 print('OpenHLS Model:')
@@ -35,13 +35,18 @@ print(model)
 #BraggNN model
 Blocks = nn.Sequential(
     ConvAttn(64,32, norm=None, act=nn.LeakyReLU(negative_slope=0.01)),
-    ConvBlock([64,32,8], [3,3], [nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01)], [None, None])
+    ConvBlock([64,32,8], [3,3], [nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01)], [None, None],img_size=9)
 )
-mlp = MLP(widths=[8*9*9,64,32,16,8,2], acts=[nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), None], norms=[None, None, None, None, None])
+mlp = MLP(widths=[200,64,32,16,8,2], acts=[nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), None], norms=[None, None, None, None, None])
 model = CandidateArchitecture(Blocks, mlp,64)
 y = model(x)
 print('Original BraggNN Model:')
 print(model)
+
+
+
+
+
 
 """
 for h in [1,8,2,4,6,10]:
