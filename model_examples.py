2da1155a8361fce81ffca2c604484fa8e74a78f2
cluster <cluster@modernintelligence.ai>
2023-11-19 11:08:57 -0800

    Global search ran for a little already, we can plot the optuna_trials.txt

--- model_examples.py
+++ model_examples.py
@@ -5,9 +5,11 @@ import torch.nn as nn
 import optuna
 from models.blocks import *
 from models.train_utils import *
+from utils.bops import *
 import time
 
 x = torch.randn((256,1,11,11))
+batch_size=256
 
 #High performing model found by search, Mean Distance: 0.203, BOPs: 17.3 Million, Param Count: 13922
 Blocks = nn.Sequential(
@@ -16,7 +18,10 @@ Blocks = nn.Sequential(
 mlp = MLP(widths=[98, 64, 8, 4, 2], acts=[nn.ReLU(), nn.GELU(), nn.GELU(), None], norms=['layer', 'batch','layer',None])
 model = CandidateArchitecture(Blocks,mlp,16)
 y = model(x)
-print('Example Model:')
+conv_bops=calculate_convblock_bops(Blocks[0], sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+mlp_bops=calculate_mlpblock_bops(mlp, sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+total_bops=conv_bops+mlp_bops
+print('Example Model w/ BOPs = ',total_bops,', Conv Bops = ', conv_bops,', MLP Bops =', mlp_bops)
 print(model)
 
 #OpenHLS Model
@@ -27,7 +32,11 @@ Blocks = nn.Sequential(
 mlp = MLP(widths=[50, 16, 8, 4, 2], acts=[nn.ReLU(), nn.ReLU(), nn.ReLU(), nn.ReLU()], norms=[None, None, None, None])
 model = CandidateArchitecture(Blocks,mlp,16)
 y = model(x)
-print('OpenHLS Model:')
+attn_bops=calculate_convattn_bops(Blocks[0], input_shape = [batch_size, 16, 9, 9], bit_width=32)
+conv_bops=calculate_convblock_bops(Blocks[1], sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+mlp_bops=calculate_mlpblock_bops(mlp, sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+total_bops=attn_bops+conv_bops+mlp_bops
+print('OpenHLS Model w/ BOPs = ',total_bops,', Attn Bops = ', attn_bops,', Conv Bops = ', conv_bops,', MLP Bops =', mlp_bops)
 print(model)
 
 
@@ -39,5 +48,9 @@ Blocks = nn.Sequential(
 mlp = MLP(widths=[200,64,32,16,8,2], acts=[nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), nn.LeakyReLU(negative_slope=0.01), None], norms=[None, None, None, None, None])
 model = CandidateArchitecture(Blocks,mlp,64)
 y = model(x)
-print('Original BraggNN Model:')
+attn_bops=calculate_convattn_bops(Blocks[0], input_shape = [batch_size, 16, 9, 9], bit_width=32)
+conv_bops=calculate_convblock_bops(Blocks[1], sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+mlp_bops=calculate_mlpblock_bops(mlp, sparsity_dict=None, weight_bit_width=32, activation_bit_width=32)
+total_bops=attn_bops+conv_bops+mlp_bops
+print('BraggNN Model w/ BOPs = ',total_bops,', Attn Bops = ', attn_bops,', Conv Bops = ', conv_bops,', MLP Bops =', mlp_bops)
 print(model)
